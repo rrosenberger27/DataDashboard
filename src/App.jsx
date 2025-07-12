9b91123fc4e1849a1153fe79c7287082011ace56
Riley <rileyrosenberger23@gmail.com>
2025-07-12 13:57:06 -0400

    some more setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import { useState } from 'react'
-import './App.css'
+import './App.css';
+import NavBar from './components/NavBar';
+import Dashboard from './components/DashBoard';
+
 
 function App() {
-  //first filtered list here will be just to ensure each item has the category
+  
 //   http://www.omdb.api.com/?apikey=YOUR_API_KEY&s=star&type=movie&page=1
 // http://www.omdb.api.com/?apikey=YOUR_API_KEY&s=star&type=movie&page=2
 // ...
-// http://www.omdb.api.com/?apikey=YOUR_API_KEY&s=star&type=movie&page=30
+// http://www.omdb.api.com/?apikey=YOUR_API_KEY&s=star&type=movie&page=20
 // then have to request initial movie like this:
-// https://www.omdbapi.com/?t=whiplash&apikey=apikey --- make sure don't run a lot otherwise will burn all requests (only have 3 a day if do this way) --- or just go simple and don't request the individual movies (would only use 20 requests per as opposed to 220)
+// https://www.omdbapi.com/?t=whiplash&apikey=apikey 
+
+//filter method - takes in genre, Imdb score, rating, year released --> each one can also be null 
+
+// OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=<api key>
+// Poster API: http://img.omdbapi.com/?i=tt3896198&h=600&apikey=<api key>
 
 
-//filter method - takes in genre, Imdb score, rating, year released
 
   return (
     <div>
+      <NavBar/>
+      <Dashboard/>
       {/* NAV BAR
       TITLE (SERIES DASHBOARD)
-      SIDEBAR - 3 broad categories - number of results
+      SIDEBAR - 3 broad categories - number of results, range of the movies (Movies 1920 - 2023)
       THE DASHBOARD ITSELF
       search bar, filters (genre, rating, decade released, Imdb score-slider on both ends)   */}
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "../styles/NavBar.css";
+
+const NavBar = () => {
+    return (
+        <div className="NavBar">
+            <h1>
+                &#127916; MovieDash!
+            </h1>
+
+            <h2>
+               &#128202; Dashboard
+            </h2>
+
+            <h2>
+                &#128269; Search
+            </h2>
+            
+            <h2>
+                &#8505;&#65039; About
+            </h2>
+        </div>
+    )
+}
+
+export default NavBar;
